6373521c52dffc61c3919b479ad5918fe296d55a
vagegast37 <sanderdamsma@gmail.com>
2017-05-10 20:35:38 +0200

    Create class2.h

---
+++ class2.h
@@ -0,0 +1,17 @@
+#ifndef _CLASS2_H_ // Include Guards
+#define _CLASS2_H_ 
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstddef>
+#include <ctime>
+#include <Windows.h>
+#include <algorithm>
+using namespace std;
+
+// Function prototypes
+extern void map(string mapnaam);
+
+#endif 
